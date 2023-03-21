7a25471ca4fb99c908041cdde62d81b1a3efdceb
whyleonardo <pleasefloyd73@gmail.com>
2023-03-21 17:32:41 -0300

    feat(ToggleThemeButton): change button style when used in a menu

--- src/app/components/Button/ToggleThemeButton/index.tsx
+++ src/app/components/Button/ToggleThemeButton/index.tsx
@@ -2,21 +2,27 @@
 
 import { useColorMode } from '@chakra-ui/react'
 import { Sun, Moon } from '@phosphor-icons/react'
+import clsx from 'clsx'
 
 export const ToggleThemeButton = ({
 	title,
-	isText
+	isText,
+	isMenuButton
 }: {
 	title?: string
 	isText?: boolean
+	isMenuButton?: boolean
 }) => {
 	const { toggleColorMode, colorMode } = useColorMode()
 
 	const isDark = colorMode === 'dark'
-
 	return (
 		<button
-			className="text-black dark:text-white dark:hover:text-yellow-500 hover:text-yellow-500 transition duration-150"
+			className={clsx(
+				'text-black dark:text-white dark:hover:text-yellow-500 hover:text-yellow-500 transition duration-150',
+				isMenuButton &&
+					'dark:text-black hover:text-white/50 dark:hover:text-white/50'
+			)}
 			onClick={toggleColorMode}
 		>
 			{isText ? (

0d982f4f2086e5c6db2eabc63aa4e2bef425b6f1
whyleonardo <pleasefloyd73@gmail.com>
2023-03-21 17:31:48 -0300

    feat(SignInButton): add conditionally to apply hover styles

--- src/app/components/Button/SignInButton/index.tsx
+++ src/app/components/Button/SignInButton/index.tsx
@@ -1,12 +1,19 @@
 'use client'
 
 import { useAuth } from '@/providers/AuthProvider'
+import clsx from 'clsx'
 
-export const SignInButton = () => {
+export const SignInButton = ({ isMenuButton }: { isMenuButton?: boolean }) => {
 	const { login, logout, currentUser } = useAuth()
 
 	return (
-		<button className="bg-red-500" onClick={!currentUser ? login : logout}>
+		<button
+			className={clsx(
+				'text-black dark:text-white transition duration-150 hover:text-black/50 dark:hover:text-white/50',
+				isMenuButton && 'hover:text-white/50 dark:hover:text-white/50'
+			)}
+			onClick={!currentUser ? login : logout}
+		>
 			{!currentUser ? 'Sign In' : 'Sign Out'}
 		</button>
 	)

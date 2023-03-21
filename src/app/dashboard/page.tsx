52c94e401dd6b130c9cf9f5d01fc9813ee730e6f
whyleonardo <pleasefloyd73@gmail.com>
2023-03-21 16:44:34 -0300

    feat: create metadata infos

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'Dashboard',
+	description: 'Dashboard page',
+	keywords: 'dashboard, page'
+}
+
 export default function DashboardPage() {
 	return (
 		<div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+import { Metadata } from 'next'
+
 import { DisplayName } from '@/app/components/Others/DisplayName'
 import { SignInButton } from 'components/Button/SignInButton'
 import { ToggleThemeButton } from 'components/Button/ToggleThemeButton'
 
+export const metadata: Metadata = {
+	title: 'Home',
+	description: 'Home page',
+	keywords: 'home, page'
+}
+
 export default function Home() {
+	const isSSR = typeof window === 'undefined'
+
+	console.log(isSSR)
+
 	return (
 		<>
 			<SignInButton />

acf380fb82ee96d0787b4ba2a3a570b9e1c66a1a
whyleonardo <pleasefloyd73@gmail.com>
2023-03-21 13:42:33 -0300

    chore: move components folder to inside app dir

--- src/components/Button/SignInButton/index.tsx
+++
@@ -1,7 +0,0 @@
-export const SignInButton = () => {
-	return (
-		<div>
-			<p> Hello World </p>
-		</div>
-	)
-}
--- src/components/Button/ToggleThemeButton/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Button, useColorMode } from '@chakra-ui/react'
-
-export const ToggleThemeButton = () => {
-	const { toggleColorMode } = useColorMode()
-
-	return <Button onClick={toggleColorMode}>Change Theme</Button>
-}

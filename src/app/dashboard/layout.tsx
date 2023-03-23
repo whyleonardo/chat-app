815ad5cf86134a7c09bba54c07387029859c73d5
whyleonardo <pleasefloyd73@gmail.com>
2023-03-23 12:22:39 -0300

    feat(dashboard): set height in dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,10 +8,10 @@ export default function DashboardLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<div className="flex flex-col space-y-2">
+		<div className="flex flex-col space-y-2 relative h-screen">
 			<Header />
 
-			<div className="flex gap-2">
+			<div className="flex gap-2 h-full">
 				<Sidebar />
 
 				<div className="border border-green-500 w-full">{children}</div>

576f905dfb34dc61b6fd53e3b0f2fe6ba36c1490
whyleonardo <pleasefloyd73@gmail.com>
2023-03-23 12:23:04 -0300

    feat(layout): set height in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 		<html>
 			<head />
 			<body
-				className={`${inter.variable} font-sans bg-brand-100 dark:bg-brand-900 text-brand-900 dark:text-brand-100`}
+				className={`${inter.variable} font-sans bg-brand-100 dark:bg-brand-900 text-brand-900 dark:text-brand-100 overflow-y-hidden`}
 			>
 				<AuthProvider>
 					<ThemeProvider>

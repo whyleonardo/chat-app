8259924e81d9898d35f50d64ec5dd4f831f7d1da
whyleonardo <pleasefloyd73@gmail.com>
2023-03-24 00:33:23 -0300

    feat(Sidebar): create sucess message toast component

--- src/app/components/Layout/Sidebar/index.tsx
+++ src/app/components/Layout/Sidebar/index.tsx
@@ -1,19 +1,19 @@
 'use client'
 
+import { useState } from 'react'
+
+import { CreateNewRoom } from 'components/Form/CreateNewRoom'
 import { Spinner } from 'components/Others/Spinner'
 
-import { useAuth } from '@/providers/AuthProvider'
 import { useRooms } from '@/queries/rooms'
-import { createRoom } from '@/services/firebase/firestore/createRoom'
-import { Plus, X as XIcon } from '@phosphor-icons/react'
+import { CheckCircle, Plus, X as XIcon } from '@phosphor-icons/react'
 import * as Dialog from '@radix-ui/react-dialog'
+import * as Toast from '@radix-ui/react-toast'
+import { clsx } from 'clsx'
 
 export const Sidebar = () => {
 	const { data, isLoading } = useRooms()
-	const { currentUser } = useAuth()
-
-	const displayName = currentUser?.displayName || ''
-	const userUid = currentUser?.uid || ''
+	const [open, setOpen] = useState(false)
 
 	return (
 		<>
@@ -28,7 +28,7 @@ export const Sidebar = () => {
 
 						<Dialog.Portal>
 							<Dialog.Overlay className=" bg-black/90 data-[state=open]:animate-overlayShow fixed inset-0" />
-							<Dialog.Content className="fixed justify-start dark:bg-brand-700 shadow-md bg-brand-200 p-4 translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%] rounded-lg">
+							<Dialog.Content className="fixed justify-start dark:bg-brand-700 shadow-md bg-brand-100 p-4 translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%] rounded-lg">
 								<div className="flex items-center justify-between">
 									<Dialog.Title className="font-bold text-lg ">
 										New Room
@@ -45,37 +45,10 @@ export const Sidebar = () => {
 								</div>
 
 								<Dialog.Description className="opacity-70 mt-[10px] mb-5 text-[15px] leading-normal">
-									Make changes to your profile here. Click save when you&apos;re
-									done.
+									Choose a name for your new room and start chatting.
 								</Dialog.Description>
 
-								<fieldset className="flex items-center gap-5">
-									<label
-										className=" text-right text-md font-bold"
-										htmlFor="name"
-									>
-										Name
-									</label>
-									<input
-										className=" focus:shadow-md inline-flex h-[35px] bg-brand-900 w-2/4 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none "
-										id="name"
-										placeholder="Room name"
-									/>
-								</fieldset>
-
-								<div className="mt-[25px] flex justify-end">
-									<Dialog.Close asChild>
-										<button
-											onClick={() =>
-												createRoom('maluquice', userUid, displayName)
-											}
-											className="text-brand-100 flex items-center gap-2 rounded-lg p-3 bg-brand-900 hover:bg-brand-800 shadow-md transition duration-150"
-										>
-											Create Room
-											<Plus weight="bold" size={18} />
-										</button>
-									</Dialog.Close>
-								</div>
+								<CreateNewRoom open={open} setOpen={setOpen} />
 							</Dialog.Content>
 						</Dialog.Portal>
 					</Dialog.Root>
@@ -108,10 +81,60 @@ export const Sidebar = () => {
 			</div>
 
 			{/* <MenuMobile /> */}
+			<SucessToast open={open} setOpen={setOpen} />
 		</>
 	)
 }
 
 const MenuMobile = () => {
 	// return <div className="sm:hidden absolute bottom-5 right-5">Menuzinho</div>
+}
+
+interface ToastProps {
+	open: boolean
+	setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const SucessToast = ({ open, setOpen }: ToastProps) => {
+	return (
+		<Toast.Provider swipeDirection={'right'}>
+			<Toast.Root
+				open={open}
+				onOpenChange={setOpen}
+				className={clsx(
+					'z-50 fixed bottom-4 inset-x-4 w-auto md:top-4 md:right-4 md:left-auto md:bottom-auto md:w-full md:max-w-sm shadow-lg rounded-lg',
+					'bg-brand-200 dark:bg-brand-800'
+				)}
+			>
+				<div className="flex">
+					<div className="w-0 flex-1 flex items-center pl-5 py-4">
+						<div className="w-full radix">
+							<Toast.Title className="flex items-center gap-2 text-sm font-medium text-brand-900 dark:text-brand-100">
+								Room is Created
+								<CheckCircle
+									size={24}
+									weight="fill"
+									className="text-green-500"
+								/>
+							</Toast.Title>
+							<Toast.Description className="mt-1 text-sm text-gray-700 dark:text-gray-400">
+								Your room is now created with sucess!
+							</Toast.Description>
+						</div>
+					</div>
+					<div className="flex">
+						<div className="flex flex-col px-3 py-2 space-y-1">
+							<div className="h-full flex items-center ">
+								<Toast.Close className="w-full border border-transparent rounded-lg h-10 px-3 py-2 flex items-center justify-center text-sm font-medium text-brand-600 dark:text-brand-400 hover:bg-brand-300/60 dark:hover:bg-brand-700 focus:z-10 focus:outline-none focus-visible:ring focus-visible:ring-brand-500 focus-visible:ring-opacity-75 transition duration-150">
+									Close
+								</Toast.Close>
+							</div>
+						</div>
+					</div>
+				</div>
+			</Toast.Root>
+			{/* TODO add viewport styles */}
+			<Toast.Viewport />
+		</Toast.Provider>
+	)
 }

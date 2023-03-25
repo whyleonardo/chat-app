c92b89f804a8cf238f5cd6c3edf89bc6f1aa27c5
whyleonardo <pleasefloyd73@gmail.com>
2023-03-25 18:51:32 -0300

    chore(deps): add tailwind prettier plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	darkMode: ['class', '[data-theme="dark"]'],
 	content: [
-		'./app/**/*.{js,ts,jsx,tsx}',
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
+		// './app/**/*.{js,ts,jsx,tsx}',
+		// './pages/**/*.{js,ts,jsx,tsx}',
+		// './components/**/*.{js,ts,jsx,tsx}',
 
 		// Or if using `src` directory:
 		'./src/**/*.{js,ts,jsx,tsx}'
@@ -12,7 +16,7 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['var(--font-inter)']
+				sans: ['var(--font-inter)', ...fontFamily.sans]
 			},
 			colors: {
 				brand: {

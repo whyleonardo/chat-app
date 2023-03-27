fc3efa0167a48c4e52cefac9b9c48b677af64eca
whyleonardo <pleasefloyd73@gmail.com>
2023-03-26 22:15:06 -0300

    feat: import Logo component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next'
 
 import { SignInButton } from 'components/Button/SignInButton'
 import { ToggleThemeButton } from 'components/Button/ToggleThemeButton'
+import { Logo } from 'components/Others/Logo'
 
 export const metadata: Metadata = {
 	title: 'Home',
@@ -11,9 +12,20 @@ export const metadata: Metadata = {
 
 export default function Home() {
 	return (
-		<>
-			<SignInButton />
-			<ToggleThemeButton />
-		</>
+		<div className="v-stack h-full">
+			<header className="h-stack center h-16 justify-between px-6">
+				<Logo />
+
+				<nav className="h-stack space-x-4">
+					<SignInButton />
+					<ToggleThemeButton />
+				</nav>
+			</header>
+
+			<div className="v-stack center border">
+				<button className="btn">Olá</button>
+				<button>Olá</button>
+			</div>
+		</div>
 	)
 }

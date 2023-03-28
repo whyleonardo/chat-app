60faaf7b610e1429d3d9321c12254b2c7594d5ee
whyleonardo <pleasefloyd73@gmail.com>
2023-03-28 12:35:14 -0300

    feat: create messages collection ref

--- src/services/firebase/firestore/collections/index.ts
+++ src/services/firebase/firestore/collections/index.ts
@@ -4,3 +4,5 @@ import { collection } from 'firebase/firestore'
 
 export const usersCollectionRef = collection(db, 'users')
 export const roomsCollectionRef = collection(db, 'rooms')
+export const messagesCollectionRef = (roomId: string) =>
+	collection(db, 'rooms', roomId, 'messages')

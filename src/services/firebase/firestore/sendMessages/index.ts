34069b1a6022bf16810758321ebcdb8267a3d874
whyleonardo <pleasefloyd73@gmail.com>
2023-03-29 17:22:16 -0300

    feat(sendMessages): add messageId field to new messages in Firestore

--- src/services/firebase/firestore/sendMessages/index.ts
+++ src/services/firebase/firestore/sendMessages/index.ts
@@ -1,6 +1,12 @@
 import { roomsCollectionRef } from '../collections'
 
-import { addDoc, collection, doc, serverTimestamp } from 'firebase/firestore'
+import {
+	addDoc,
+	collection,
+	doc,
+	serverTimestamp,
+	updateDoc
+} from 'firebase/firestore'
 
 export async function sendMessage(
 	roomId: string,
@@ -10,9 +16,13 @@ export async function sendMessage(
 	const roomDocRef = doc(roomsCollectionRef, roomId)
 	const messageRef = collection(roomDocRef, 'messages')
 
-	await addDoc(messageRef, {
+	const newMessageRef = await addDoc(messageRef, {
 		text: text,
 		timestamp: serverTimestamp(),
 		sender: userId
+	})
+
+	await updateDoc(newMessageRef, {
+		messageId: newMessageRef.id
 	})
 }
